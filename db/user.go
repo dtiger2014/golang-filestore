bae0fa7abee4522ebbf53f8bd3203dcc3014213c
fanfan <piaolongfan@xiimoon.com>
2019-06-04 18:14:39 +0800

    add db

---
+++ db/user.go
@@ -0,0 +1,68 @@
+package db
+
+import (
+	"fmt"
+	mydb "golang-filestore/db/mysql"
+)
+
+func UserSignup(username string, passwd string) bool {
+	stmt, err := mydb.DBConn().Prepare(
+		"insert ignore into tbl_user (`user_name`,`user_pwd`) values(?,?)")
+	if err != nil {
+		fmt.Println("Failed to insert, err:" + err.Error())
+		return false
+	}
+	defer stmt.Close()
+
+	ret, err := stmt.Exec(username, passwd)
+	if err != nil {
+		fmt.Println("Failed to insert, err:" + err.Error())
+		return false
+	}
+	if rowAffected, err := ret.RowsAffected(); err == nil && rowAffected > 0 {
+		return true
+	}
+	return false
+}
+func UserSignin(username string, encpwd string) bool {
+	stmt, err := mydb.DBConn().Prepare(
+		"select * from tbl_user where user_name=? limit 1")
+	if err != nil {
+		fmt.Println(err.Error())
+		return false
+	}
+	defer stmt.Close()
+
+	rows, err := stmt.Query(username)
+	if err != nil {
+		fmt.Println(err.Error())
+		return false
+	} else if rows == nil {
+		fmt.Println("username not found: " + username)
+		return false
+	}
+
+	pRows := mydb.ParseRows(rows)
+	if len(pRows) > 0 && string(pRows[0]["user_pwd"].([]byte)) == encpwd {
+		return true
+	}
+
+	return false
+}
+
+func UpdateToken(username string, token string) bool {
+	stmt, err := mydb.DBConn().Prepare(
+		"replace into tbl_user_token (`user_name`,`user_token`) values(?,?)")
+	if err != nil {
+		fmt.Println(err.Error())
+		return false
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Exec(username, token)
+	if err != nil {
+		fmt.Println(err.Error())
+		return false
+	}
+	return true
+}
